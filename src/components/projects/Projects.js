1d26e4be222aa3f733fe06328aecab6f110c3b95
ShimiyonS <shimjnss@gmail.com>
2025-06-03 14:32:04 +0530

    update

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -17,16 +17,25 @@ const Projects = () => {
       </div>
       <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-6 xl:gap-14">
         <ProjectsCard
+          title="MONEY MANAGER APP"
+          des=" A financial tracking app built with React, Node.js, and MongoDB — track expenses, manage budgets, and visualize stats."
+          src={projectProfile}
+          link="https://money-manager-app-5ed7.vercel.app"
+          githubLink="https://github.com/ShimiyonS/Money_manager_app"
+        />
+        <ProjectsCard
+          title="Portfolios (Web and Mobile apps)"
+          des=" Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+              Explicabo quibusdam voluptate sapiente voluptatibus harum quidem!"
+          src={projectTwo}
+          type="protfolio"
+          profilelink="https://drive.google.com/drive/u/0/folders/1G-bAU7RqtlagrcJOhCprUldHpnQh4stU"
+        />
+        <ProjectsCard
           title="SOCIAL MEDIA CLONE"
           des=" Lorem, ipsum dolor sit amet consectetur adipisicing elit.
               Explicabo quibusdam voluptate sapiente voluptatibus harum quidem!"
           src={projectOne}
-        />
-        <ProjectsCard
-          title="E-commerce Website"
-          des=" Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-              Explicabo quibusdam voluptate sapiente voluptatibus harum quidem!"
-          src={projectTwo}
         />
         {/* <ProjectsCard
           title="Chatting App"
@@ -34,13 +43,7 @@ const Projects = () => {
               Explicabo quibusdam voluptate sapiente voluptatibus harum quidem!"
           src={projectThree}
         /> */}
-        <ProjectsCard
-          title="MONEY MANAGER APP"
-          des=" A financial tracking app built with React, Node.js, and MongoDB — track expenses, manage budgets, and visualize stats."
-          src={projectProfile}
-          link="https://money-manager-app-5ed7.vercel.app"
-          githubLink="https://github.com/ShimiyonS/Money_manager_app"
-        />
+
         {/* <ProjectsCard
           title="E-commerce Website"
           des=" Lorem, ipsum dolor sit amet consectetur adipisicing elit.
